61d924c81ff1437bd860fe98d14f5aa0cf3692c6
vrushabh-digraje <vrushabhdigraje07@gmail.com>
2025-07-12 15:07:58 +0530

    Update ApiRequest.js

--- Frontend/src/utils/ApiRequest.js
+++ Frontend/src/utils/ApiRequest.js
@@ -1,4 +1,4 @@
-const host = "http://localhost:4000";
+const host = "https://personal-finance-manager-backend.onrender.com";
 export const setAvatarAPI = `${host}/api/auth/setAvatar`;
 export const registerAPI = `${host}/api/auth/register`;
 export const loginAPI = `${host}/api/auth/login`;
